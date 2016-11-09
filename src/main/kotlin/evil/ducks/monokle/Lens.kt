6344b98f133c0eb736cd7846fb9ffe8e18e1738a
Tony Lawrence <code@tonylawrence.com>
2016-11-09 09:03:01 +0000

    extract out Lens and start to test get

---
+++ src/main/kotlin/evil/ducks/monokle/Lens.kt
@@ -0,0 +1,5 @@
+package evil.ducks.monokle
+
+class Lens<A, B>(val get: (A) -> B, private val set: (B) -> (A) -> A)
+
+//infix fun Lens<A, B>.composeLens(l: Lens<B, C>) : Lens<A, C> = l
--- src/test/kotlin/evil/ducks/monokle/ExampleTest.kt
+++ src/test/kotlin/evil/ducks/monokle/ExampleTest.kt
@@ -1,16 +1,38 @@
+@file:Suppress("ConvertLambdaToReference")
+
 package evil.ducks.monokle
 
+import evil.ducks.monokle.ExampleLens._address
+import evil.ducks.monokle.ExampleLens._company
+import evil.ducks.monokle.ExampleLens._street
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
-class ExampleTest {
-    val employee = Employee("Tony", Company("Evil Ducks", Address(22, Street("Acacia Avenue"))))
+object ExampleLens {
+    val _company = Lens(Employee::company, { c -> { e -> e.copy(company = c) } })
+    val _address = Lens(Company::address, { a -> { c -> c.copy(address = a) } })
+    val _street  = Lens(Address::street, { s -> { a -> a.copy(street = s) } })
 
-    @Test fun `let us start by accessing some simple data classes`() {
-        assertThat(employee.name, equalTo("Tony"))
-        assertThat(employee.company.name, equalTo("Evil Ducks"))
-        assertThat(employee.company.address.number, equalTo(22))
-        assertThat(employee.company.address.street.name, equalTo("Acacia Avenue"))
+//    val _companyAddress = _company composeLens _address
+}
+
+class ExampleTest {
+    val acaciaAvenue = Street("Acacia Avenue")
+    val evilDucksBuilding = Address(22, acaciaAvenue)
+    val evilDucks = Company("Evil Ducks", evilDucksBuilding)
+    val tony = Employee("Tony", evilDucks)
+
+    @Test fun `accessing some simple data classes is tedious`() {
+        assertThat(tony.name, equalTo("Tony"))
+        assertThat(tony.company.name, equalTo("Evil Ducks"))
+        assertThat(tony.company.address.number, equalTo(22))
+        assertThat(tony.company.address.street.name, equalTo("Acacia Avenue"))
+    }
+
+    @Test fun `using a simple data class lens makes light work`() {
+        assertThat(_company.get(tony), equalTo(evilDucks))
+        assertThat(_address.get(evilDucks), equalTo(evilDucksBuilding))
+        assertThat(_street.get(evilDucksBuilding), equalTo(acaciaAvenue))
     }
 }
